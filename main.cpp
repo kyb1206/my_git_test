44335445693c70103b6e98f41bc0b4656cf39549
my_git_test <21800129@handong.edu>
2020-04-16 02:28:39 -0700

    hw2-2 done

--- main.cpp
+++ main.cpp
@@ -4,5 +4,19 @@ int sorted[10];
 
 int main(){
 
+    int num[10];
+
+    for(int i=0; i<10; i++){
+        cin>>num[i];   
+    }
+
+    //merge(num, 0, 5, 9);
+    merge_sort(num, 0, 9);
+    
+    for(int i=0; i<10; i++){
+        cout<<sorted[i]<<" ";
+    }
+    cout<<endl;
+
     return 0;
 }
